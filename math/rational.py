8ad8c768508f9a3afdfd1009ffbce35736f1696e
gaster618 <gaster618@gmail.com>
2018-02-20 11:37:29 +0300

    math rational

---
+++ math/rational.py
@@ -0,0 +1,9 @@
+class Rational:
+	def __init__(self,numinator,denominator):
+		self.numinator = numinator
+		self.denominator = denominator
+
+
+	def __cmp__(self, other):
+		other.numinator = numinator
+			
